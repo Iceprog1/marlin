762678c0504440003416287695ce0965fa3f5154
Iceprog1 <iceprog1@gmail.com>
2023-11-25 07:53:17 +0300

    Начал дипломный проект

---
+++ Дипломный проект/functions.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function get_user_by_email($email){
+    $pdo = new PDO('mysql:host=localhost;dbname=database', 'root', '');
+    $sql = 'SELECT * FROM diplom WHERE email=:email';
+    $state = $pdo->prepare($sql);
+    $state->execute(['email' => $email]);
+    $user = $state->fetch(PDO::FETCH_ASSOC);
+    return $user;
+}
+
+function set_flash_message($name, $message){
+    $_SESSION[$name] = $message;
+}
+
+function redirect_to($path){
+    header("Location: {$path}");
+    exit();
+}
+
+function add_user($email, $password){
+    $pdo = new PDO('mysql:host=localhost;dbname=database', 'root', '');
+    $sql = 'INSERT INTO diplom (email, password) VALUES (:email, :password)';
+    $state = $pdo->prepare($sql);
+    $state->execute(['email' => $email, 'password' => password_hash($password, PASSWORD_DEFAULT)]);
+}
+
+function display_flash_message($name){
+    if (isset($_SESSION[$name])){
+        echo "<div class=\"alert alert-{$name} text-dark\" role=\"alert\"> {$_SESSION[$name]}</div>";
+        unset($_SESSION[$name]);
+    }
+}
--- Синтаксис ООП/index.php
+++ Синтаксис ООП/index.php
@@ -1,1 +1,68 @@
 <?php
+
+//Это создание класса, оно начинается со слова class
+class Person {
+    // У класса бывают свойства, они создаются так
+    // Свойства это информация, методы это действия
+    public $name;
+    public $age;
+
+    // Значение константы пока не меняется
+    const ID = 10;
+
+
+    // Чтобы создать методы мы пишем
+    // В скобках наш метод принимает параметр из вызова методов ниже в которые мы передали значение параметров
+    // Чтобы что-то вернуть отсюда мы пишем return
+    public function sayHello(){
+        return 'Hi, i am' . $this->name . '' . $this->setName('derk');
+    }
+
+    // То имя которое внизу мы передали как параметр, здесь мы присваиваем свойству текущего нижнего объекта
+    public function setName($name){
+        $this->name = $name;
+    }
+
+    public function setAge($age){
+        $this->age = $age;
+    }
+
+    // Если у нашей функции мы ставим солов static, что внизу можем вызывать этот метод, просто указав имя класса
+    public static function saySomthing(){
+        echo 'bla-bla';
+        // в статичной функции нельзя использовать $this
+    }
+}
+
+
+
+// При помощи такого класса, мы можем создать объект
+// Чтобы создать объект мы пишем
+$myPerson = new Person();
+
+Person::saySomthing();
+// Чтобы вызвать метод из объекта $myPerson дописываем вот это
+//$myPerson->sayHello();
+
+// Также можно обращаться к методам объекта вот так
+//$myPerson->name = 'Adam';
+
+// После создания такого объекта
+// Этот объект уже считается отдельным объектом
+// Мы можем передавать разные параметры в метод из объекта при этом вызывая один и тот же объект
+$myPerson2 = new Person();
+
+//$myPerson->sayHello(' Adam');
+//echo '<pre>';
+//$myPerson2->sayHello(' Derk');
+
+//$myPerson2->setName('Alisa');
+//$myPerson2->setAge('11');
+
+$myPerson2->sayHello();
+
+echo $myPerson2->name;
+echo $myPerson2->age;
+
+
+echo $myPerson2::ID;
